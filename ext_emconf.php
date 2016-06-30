1310f98075a0c8e9a4efb7e28f6d9f14b4cf999e
Marieke de Bruin <marieke@maxserv.com>
2016-06-30 10:34:56 +0200

    Added ext_emconf

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Yoast SEO',
+	'description' => '',
+	'category' => 'fe',
+	'author' => 'Yoast SEO for TYPO3',
+	'author_company' => 'MaxServ B.V., Yoast',
+	'author_email' => '',
+	'clearCacheOnLoad' => 0,
+	'dependencies' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+	'autoload' => array(
+		'psr-4' => array('YoastSeoForTypo3\\YoastSeo\\' => 'Classes')
+	),
+	'conflicts' => '',
+	'suggests' => array(),
+);
