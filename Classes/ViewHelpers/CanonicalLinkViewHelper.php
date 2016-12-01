3d01db8b6f6027d2df2296568f63d24c745009fc
Arno Schoon <arno@maxserv.com>
2016-12-01 14:57:10 +0100

    Removed tag services, use FLUIDTEMPLATE instead for rendering. Configuration is TypoScript based and using Fluid standalone view integrates better with TYPO3 CMS.

---
+++ Classes/ViewHelpers/CanonicalLinkViewHelper.php
@@ -0,0 +1,50 @@
+<?php
+namespace YoastSeoForTypo3\YoastSeo\ViewHelpers;
+
+use TYPO3\CMS\Core;
+use TYPO3\CMS\Fluid;
+use TYPO3\CMS\Frontend;
+
+/**
+ * Class CanonicalLinkViewHelper
+ *
+ * @package YoastSeoForTypo3\YoastSeo\ViewHelpers
+ */
+class CanonicalLinkViewHelper extends Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
+{
+
+    /**
+     * Name of the tag to be created by this view helper
+     *
+     * @var string
+     */
+    protected $tagName = 'link';
+
+    /**
+     * @param string $href
+     * 
+     * @return string
+     */
+    public function render($href = null)
+    {
+        $this->tag->addAttribute('rel', 'canonical');
+
+        if (!empty($href) && Core\Utility\GeneralUtility::isValidUrl($href)) {
+            $this->tag->addAttribute('href', $href);
+        } elseif ($GLOBALS['TSFE'] instanceof Frontend\Controller\TypoScriptFrontendController
+            && $GLOBALS['TSFE']->contentPid > 0
+        ) {
+            $uriBuilder = $this->controllerContext->getUriBuilder();
+
+            $uri = $uriBuilder->reset()
+                ->setCreateAbsoluteUri(true)
+                ->setTargetPageUid($GLOBALS['TSFE']->contentPid)
+                ->build();
+
+            $this->tag->addAttribute('href', $uri);
+        }
+
+        return $this->tag->hasAttribute('href') ? $this->tag->render() : '';
+    }
+
+}
