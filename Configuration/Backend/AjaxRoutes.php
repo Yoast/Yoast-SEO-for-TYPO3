f7d683f906f7710ed91aa755906d7fcd96f84840
Arno Schoon <arno@maxserv.com>
2016-12-01 10:54:31 +0100

    Remove unused PageContentController.

--- Configuration/Backend/AjaxRoutes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    'yoast_seo-page-content-preview' => array(
-        'path' => '/yoast_seo/page-content/preview',
-        'target' => \YoastSeoForTypo3\YoastSeo\Backend\Controller\PageContentController::class . '::renderPagePreview'
-    )
-);
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,16 +4,23 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php'][
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook'][] = \YoastSeoForTypo3\YoastSeo\Backend\PageLayoutHeader::class . '->render';
 
 /** @var \YoastSeoForTypo3\YoastSeo\Frontend\PageRenderer\PageMetaRenderer $pageMetaRenderer */
-$pageMetaRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\YoastSeoForTypo3\YoastSeo\Frontend\PageRenderer\PageMetaRenderer::class);
+$pageMetaRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \YoastSeoForTypo3\YoastSeo\Frontend\PageRenderer\PageMetaRenderer::class
+);
 
-$pageMetaRenderer->registerService(\YoastSeoForTypo3\YoastSeo\Frontend\MetaService\CanonicalTagService::class);
-$pageMetaRenderer->registerService(\YoastSeoForTypo3\YoastSeo\Frontend\MetaService\SocialTagService::class);
+$pageMetaRenderer->registerService(
+    \YoastSeoForTypo3\YoastSeo\Frontend\MetaService\CanonicalTagService::class
+);
+$pageMetaRenderer->registerService(
+    \YoastSeoForTypo3\YoastSeo\Frontend\MetaService\SocialTagService::class
+);
 
 unset($pageMetaRenderer);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
-    config.yoast_seo.fe_preview_type = ' . \YoastSeoForTypo3\YoastSeo\Backend\PageLayoutHeader::FE_PREVIEW_TYPE . '
-');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(
+    'config.yoast_seo.fe_preview_type = '
+        . \YoastSeoForTypo3\YoastSeo\Backend\PageLayoutHeader::FE_PREVIEW_TYPE
+);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
     'YoastSeo',
