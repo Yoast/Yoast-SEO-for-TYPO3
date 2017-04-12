71a54bf6e4148d948576ac92364dd3ace499836d
Richard Haeser <richard@maxserv.com>
2017-04-12 22:39:27 +0200

    [TASK] Set version to 1.0.0-RC in extension configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = array(
     'dependencies' => '',
     'state' => 'stable',
     'uploadfolder' => 0,
-    'version' => '0.0.1',
+    'version' => '1.0.0-RC',
     'constraints' => array(
         'depends' => array(
         ),
