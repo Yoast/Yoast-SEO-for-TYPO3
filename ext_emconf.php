7d873b09e494c090e626eb96737ca59a40c0aa48
Richard Haeser <richard@maxserv.com>
2017-04-19 23:46:57 +0200

    [TASK] Updated TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,6 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
+            'typo3' => '7.6.13-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
