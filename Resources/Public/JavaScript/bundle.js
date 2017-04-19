bc84a9f27f18111ad03d8235d48b2bce25b1278f
Richard Haeser <richard@maxserv.com>
2017-04-19 21:08:26 +0200

    [TASK] Update Yoast SEO js

--- Resources/Public/JavaScript/bundle.js
+++ Resources/Public/JavaScript/bundle.js
@@ -24210,7 +24210,7 @@ module.exports = {
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.1';
+  var VERSION = '4.17.4';
 
   /** Used as references for various `Number` constants. */
   var INFINITY = 1 / 0;
@@ -24325,8 +24325,7 @@ module.exports = {
     if (value == null) {
       return value === undefined ? undefinedTag : nullTag;
     }
-    value = Object(value);
-    return (symToStringTag && symToStringTag in value)
+    return (symToStringTag && symToStringTag in Object(value))
       ? getRawTag(value)
       : objectToString(value);
   }
