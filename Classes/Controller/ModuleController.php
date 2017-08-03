a2186465c286f8bac51edd6ec6d88dde03c835fa
Richard Haeser <richard@maxserv.com>
2017-08-03 10:46:05 +0200

    [TASK] Remove extra enters

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -582,9 +582,7 @@ class ModuleController extends ActionController
                     ))
                     ->setShowLabelText(true);
 
-
                 $buttonBar->addButton($saveButton, ButtonBar::BUTTON_POSITION_LEFT, 2);
-
             }
         }
         $shortcutButton = $buttonBar->makeShortcutButton()
