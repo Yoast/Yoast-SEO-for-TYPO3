b503f32e78fa95b79cdfa58456314bc162c7fa99
Richard Haeser <richard@maxserv.com>
2017-10-17 23:10:35 +0200

    [FEATURE] Added possibility to hide the analysis in Page module

--- ext_tables.php
+++ ext_tables.php
@@ -15,4 +15,12 @@ if (TYPO3_MODE === 'BE') {
             'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/BackendModule.xlf',
         )
     );
+
+    // Extend user settings
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['hideYoastInPageModule'] = [
+        'label' => 'LLL:EXT:yoast_seo/Resources/Private/Language/BackendModule.xlf:usersettings.hideYoastInPageModule',
+        'type' => 'check'
+    ];
+    $GLOBALS['TYPO3_USER_SETTINGS']['showitem'] .= ',
+            --div--;LLL:EXT:yoast_seo/Resources/Private/Language/BackendModule.xlf:usersettings.title,hideYoastInPageModule';
 }
