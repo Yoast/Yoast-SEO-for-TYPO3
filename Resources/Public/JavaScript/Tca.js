6c481cc2be07ea7f43c9feb80e0a8194397e0d40
Richard Haeser <richardhaeser@gmail.com>
2017-12-30 15:01:39 +0100

    [TASK] Progressbars

--- Resources/Public/JavaScript/Tca.js
+++ Resources/Public/JavaScript/Tca.js
@@ -33,11 +33,19 @@ define(['jquery', './bundle', 'TYPO3/CMS/Backend/AjaxDataHandler', 'TYPO3/CMS/Ba
                 },
                 targetElement: $snippetPreview.get(0),
                 callbacks: {
+                    saveSnippetData: function() {
+                        setTimeout(function() {
+                            updateProgressBars();
+                        }, 500);
+                    }
                 }
             });
 
             var app = new YoastSEO.App({
                 snippetPreview: snippetPreview,
+                elementTargets: [
+                    'id-of-title-element'
+                ],
                 targets: {
                 },
                 callbacks: {
@@ -54,7 +62,33 @@ define(['jquery', './bundle', 'TYPO3/CMS/Backend/AjaxDataHandler', 'TYPO3/CMS/Ba
                 },
                 locale: $metaSection.find('locale').text()
             });
+
+            $("*[data-formengine-input-name='" + $titleTcaSelector + "']").on('input', function() {
+                var $titleElement = $targetElement.find('#snippet-editor-title');
+                $titleElement.val($(this).val());
+                snippetPreview.changedInput();
+            });
+
+            $("*[data-formengine-input-name='" + $descriptionTcaSelector + "']").on('input', function() {
+                var $descriptionElement = $targetElement.find('#snippet-editor-meta-description');
+                $descriptionElement.val($(this).val());
+                snippetPreview.changedInput();
+            });
+
+            $("*[data-formengine-input-name='" + $titleTcaSelector + "']").parent('.form-control-clearable').after("<progress id='yoast-progress-title' class='yoast-progressbars'></progress>");
+            $("*[data-formengine-input-name='" + $descriptionTcaSelector + "']").after("<progress id='yoast-progress-description' class='yoast-progressbars'></progress>");
+
+            updateProgressBars();
+
         });
+
+        function updateProgressBars() {
+            $('#yoast-progress-title').attr('max', $('progress.snippet-editor__progress-title').attr('max'));
+            $('#yoast-progress-title').val($('progress.snippet-editor__progress-title').val());
+
+            $('#yoast-progress-description').attr('max', $('progress.snippet-editor__progress-meta-description').attr('max'));
+            $('#yoast-progress-description').val($('progress.snippet-editor__progress-meta-description').val());
+        }
 
         previewRequest.fail(function (jqXHR) {
             var text = 'We got an error ' + jqXHR.status + ' (' + jqXHR.statusText + ') when requesting ' + previewUrl + ' to analyse your content. Please check your javascript console for more information.';
