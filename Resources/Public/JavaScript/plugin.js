7fde1fcb590f8ef78d455937e72f4a8c9e592870
Richard Haeser <richard@maxserv.com>
2018-03-01 21:43:22 +0100

    [TASK] Change JS bundle

--- Resources/Public/JavaScript/plugin.js
+++ Resources/Public/JavaScript/plugin.js
@@ -2,6 +2,8 @@ var oldDefineAmd = define.amd;
 define.amd = false;
 var SnippetPreview = require( "yoastseo" ).SnippetPreview;
 var App = require( "yoastseo" ).App;
+var Paper = require( "yoastseo" ).value;
+var Researcher = require( "yoastseo" ).Researcher;
 define.amd = oldDefineAmd;
 var scoreToRating = require( 'yoastseo' ).helpers.scoreToRating;
 var debounce = require('lodash/debounce');
@@ -10,7 +12,9 @@ define(function () {
     return {
         'App': App,
         'debounce': debounce,
+        'Paper': Paper,
         'SnippetPreview': SnippetPreview,
+        'Researcher': Researcher,
         'scoreToRating': scoreToRating
     };
 });
