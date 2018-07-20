72ff924d26b38bda5a7e3f70078191359e97e523
Richard Haeser <richard@maxserv.com>
2018-07-20 12:01:51 +0200

    [TASK] Added description for TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Yoast SEO for TYPO3',
-    'description' => '',
+    'description' => 'Optimise your website for search engines with Yoast SEO for TYPO3. With this extension you get all the technical SEO stuff you need and will help editors to write high quality content.',
     'category' => 'misc',
     'author' => 'MaxServ / Yoast',
     'author_company' => 'MaxServ B.V., Yoast',
