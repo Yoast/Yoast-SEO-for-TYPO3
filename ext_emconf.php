fccbd45a7d710055ac9cb54cdea8757baf28bb25
Richard Haeser <richard@maxserv.com>
2018-10-02 20:52:16 +0200

    [TASK] Update v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.0.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.0.0-9.5.99',
+            'typo3' => '9.5.0-9.5.99',
             'seo' => ''
         ),
         'conflicts' => array(),
