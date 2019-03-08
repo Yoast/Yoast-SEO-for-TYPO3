a32e3838888a1d1d6d99fa9efeb0bc5c2f3f35c0
Frank Baak <frank@maxserv.com>
2019-03-08 16:24:16 +0100

    Locked all package versions + use the new wordsToHighlight prop instead of keyword

--- Resources/Public/JavaScript/Components/SnippetPreview.js
+++ Resources/Public/JavaScript/Components/SnippetPreview.js
@@ -44,7 +44,7 @@ function mapStateToProps (state) {
     return {
         ...state.content,
         ...state.analysis,
-        keyword: state.focusKeyword
+        wordsToHighlight: [state.focusKeyword]
     }
 }
 
