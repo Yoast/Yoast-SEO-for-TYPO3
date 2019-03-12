5c0734eea1a15a434feec104c6a3af7ae02c6ca1
Richard Haeser <richard@maxserv.com>
2019-03-12 22:16:28 +0100

    Removed unused use statement

--- Classes/Form/Element/Insights.php
+++ Classes/Form/Element/Insights.php
@@ -17,7 +17,6 @@ namespace YoastSeoForTypo3\YoastSeo\Form\Element;
 use TYPO3\CMS\Backend\Form\AbstractNode;
 use TYPO3\CMS\Backend\Form\NodeFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\PathUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
