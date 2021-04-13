f491526c8073ffba030bde77271f6068c90f5b2b
Riny van Tiggelen <info@online-gamer.nl>
2021-04-13 22:21:22 +0200

    [TASK] Divided UrlService getPreviewUrl method and implemented SingletonInterface to set the generatedUri for public use

--- Classes/Service/UrlService.php
+++ Classes/Service/UrlService.php
@@ -2,11 +2,14 @@
 declare(strict_types=1);
 namespace YoastSeoForTypo3\YoastSeo\Service;
 
+use Psr\Http\Message\UriInterface;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Domain\Repository\PageRepository;
+use TYPO3\CMS\Core\Exception\SiteNotFoundException;
 use TYPO3\CMS\Core\Routing\RouteNotFoundException;
+use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -15,12 +18,17 @@ use YoastSeoForTypo3\YoastSeo\Utility\YoastUtility;
 /**
  * Class UrlService
  */
-class UrlService
+class UrlService implements SingletonInterface
 {
     /**
      * @var \TYPO3\CMS\Backend\Routing\UriBuilder
      */
     protected $uriBuilder;
+
+    /**
+     * @var \Psr\Http\Message\UriInterface|null
+     */
+    protected $generatedUri;
 
     /**
      * UrlService constructor.
@@ -43,25 +51,13 @@ class UrlService
         int $languageId,
         $additionalGetVars = ''
     ): string {
-        $rootLine = BackendUtility::BEgetRootLine($pageId);
-        // Mount point overlay: Set new target page id and mp parameter
-        $pageRepository = $this->getPageRepository();
-        $finalPageIdToShow = $pageId;
-        $mountPointInformation = $pageRepository->getMountPointInfo($pageId);
-        if ($mountPointInformation && $mountPointInformation['overlay']) {
-            // New page id
-            $finalPageIdToShow = $mountPointInformation['mount_pid'];
-            $additionalGetVars .= '&MP=' . $mountPointInformation['MPvar'];
-        }
+        $this->checkMountpoint($pageId, $additionalGetVars);
+        $rootLine = $this->getRootLine($pageId);
+        $site = $this->getSite($pageId, $rootLine);
 
-        $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
-        $site = $siteFinder->getSiteByPageId($finalPageIdToShow, $rootLine);
-        if ($site instanceof Site) {
-            $additionalQueryParams = [];
-            parse_str($additionalGetVars, $additionalQueryParams);
-            $additionalQueryParams['_language'] = $site->getLanguageById($languageId);
+        if ($site !== null) {
             $uriToCheck = YoastUtility::fixAbsoluteUrl(
-                (string)$site->getRouter()->generateUri($finalPageIdToShow, $additionalQueryParams)
+                (string)$this->generateUri($site, $pageId, $languageId, $additionalGetVars)
             );
 
             if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][self::class]['urlToCheck'])) {
@@ -69,7 +65,7 @@ class UrlService
                     $_params = [
                         'urlToCheck' => $uriToCheck,
                         'site' => $site,
-                        'finalPageIdToShow' => $finalPageIdToShow,
+                        'finalPageIdToShow' => $pageId,
                         'languageId' => $languageId
                     ];
 
@@ -77,13 +73,68 @@ class UrlService
                 }
             }
             $uri = (string)$this->uriBuilder->buildUriFromRoute('ajax_yoast_preview', [
-                'uriToCheck' => $uriToCheck, 'pageId' => $finalPageIdToShow
+                'uriToCheck' => $uriToCheck, 'pageId' => $pageId
             ]);
         } else {
-            $uri = BackendUtility::getPreviewUrl($finalPageIdToShow, '', $rootLine, '', '', $additionalGetVars);
+            $uri = BackendUtility::getPreviewUrl($pageId, '', $rootLine, '', '', $additionalGetVars);
         }
 
         return $uri;
+    }
+
+    /**
+     * @param int $pageId
+     * @param     $additionalGetVars
+     */
+    public function checkMountPoint(int &$pageId, &$additionalGetVars): void
+    {
+        $pageRepository = $this->getPageRepository();
+        $mountPointInformation = $pageRepository->getMountPointInfo($pageId);
+        if ($mountPointInformation && $mountPointInformation['overlay']) {
+            // New page id
+            $pageId = $mountPointInformation['mount_pid'];
+            $additionalGetVars .= '&MP=' . $mountPointInformation['MPvar'];
+        }
+    }
+
+    /**
+     * @param int $pageId
+     * @return array
+     */
+    public function getRootLine(int $pageId): array
+    {
+        return BackendUtility::BEgetRootLine($pageId);
+    }
+
+    /**
+     * @param int   $pageId
+     * @param array $rootLine
+     * @return \TYPO3\CMS\Core\Site\Entity\Site|null
+     */
+    public function getSite(int $pageId, array $rootLine): ?Site
+    {
+        $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+        try {
+            return $siteFinder->getSiteByPageId($pageId, $rootLine);
+        } catch (SiteNotFoundException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param \TYPO3\CMS\Core\Site\Entity\Site $site
+     * @param int                              $pageId
+     * @param int                              $languageId
+     * @param string                           $additionalGetVars
+     * @return \Psr\Http\Message\UriInterface
+     * @throws \TYPO3\CMS\Core\Routing\InvalidRouteArgumentsException
+     */
+    public function generateUri(Site $site, int $pageId, int $languageId, $additionalGetVars = ''): UriInterface
+    {
+        $additionalQueryParams = [];
+        parse_str($additionalGetVars, $additionalQueryParams);
+        $additionalQueryParams['_language'] = $site->getLanguageById($languageId);
+        return $this->generatedUri = $site->getRouter()->generateUri($pageId, $additionalQueryParams);
     }
 
     /**
@@ -108,6 +159,14 @@ class UrlService
     public function getUrlForType(int $type, $additionalGetParameters = ''): string
     {
         return GeneralUtility::getIndpEnv('TYPO3_SITE_PATH') . '?type=' . $type . $additionalGetParameters;
+    }
+
+    /**
+     * @return \Psr\Http\Message\UriInterface|null
+     */
+    public function getGeneratedUri(): ?UriInterface
+    {
+        return $this->generatedUri;
     }
 
     /**
