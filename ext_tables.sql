ab045d1d6a8928439d7eda96ad55e28710267ea8
Yann Weyer <yann.weyer@tu-berlin.de>
2021-06-04 17:20:43 +0200

    Remove columns for virtual fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,9 +8,6 @@ CREATE TABLE pages (
 	tx_yoastseo_cornerstone tinyint(3) DEFAULT '0' NOT NULL,
 	tx_yoastseo_score_readability varchar(50) DEFAULT '' NOT NULL,
 	tx_yoastseo_score_seo varchar(50) DEFAULT '' NOT NULL,
-	tx_yoastseo_snippetpreview tinyint(3) DEFAULT '0' NOT NULL,
-	tx_yoastseo_focuskeyword_analysis tinyint(3) DEFAULT '0' NOT NULL,
-	tx_yoastseo_readability_analysis tinyint(3) DEFAULT '0' NOT NULL,
 	tx_yoastseo_focuskeyword_premium int(11) DEFAULT '0' NOT NULL,
 	KEY tx_yoastseo_cornerstone (tx_yoastseo_cornerstone),
 );
