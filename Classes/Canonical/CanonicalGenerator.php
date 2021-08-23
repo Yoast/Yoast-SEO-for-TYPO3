f34a5790b1fdc7bcfe1916d12752bd04bb2c9e7f
Pablo Câmara <46443482+Pablo-Camara@users.noreply.github.com>
2021-08-23 15:18:09 +0100

    extra check

--- Classes/Canonical/CanonicalGenerator.php
+++ Classes/Canonical/CanonicalGenerator.php
@@ -40,15 +40,17 @@ class CanonicalGenerator
         if ($typoScriptFrontendController === null) {
             $typoScriptFrontendController = $this->getTypoScriptFrontendController();
 
-            if (empty($typoScriptFrontendController->page)) {
-                $typoScriptFrontendController->getPageAndRootLine();
-            }
+            if (!is_null($typoScriptFrontendController)) {
+                if (empty($typoScriptFrontendController->page)) {
+                    $typoScriptFrontendController->getPageAndRootLine();
+                }
 
-            if (!is_object($this->typoScriptFrontendController->cObj)) {
-                $typoScriptFrontendController->initTemplate();
-                $typoScriptFrontendController->getConfigArray();
-                $typoScriptFrontendController->settingLanguage();
-                $typoScriptFrontendController->newCObj();
+                if (!is_object($this->typoScriptFrontendController->cObj)) {
+                    $typoScriptFrontendController->initTemplate();
+                    $typoScriptFrontendController->getConfigArray();
+                    $typoScriptFrontendController->settingLanguage();
+                    $typoScriptFrontendController->newCObj();
+                }
             }
         }
         if ($signalSlotDispatcher === null) {
