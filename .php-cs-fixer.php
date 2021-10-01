2c95bbb689c999be2cb5f44cbadd5094e322b480
Riny van Tiggelen <riny@maxserv.com>
2021-10-01 14:34:35 +0200

    [TASK] Updated php-cs-fixer configuration to prepare for php-cs-fixer 3.0

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -20,12 +20,22 @@ if (PHP_SAPI !== 'cli') {
     die('This script supports command line usage only. Please check your command.');
 }
 
-return PhpCsFixer\Config::create()
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->exclude('node_modules')
+    ->exclude('public')
+    ->exclude('var')
+    ->in(__DIR__);
+
+$config = new PhpCsFixer\Config();
+$config
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
         'general_phpdoc_annotation_remove' => [
-            'author'
+            'annotations' => [
+                'author'
+            ]
         ],
         'no_leading_import_slash' => true,
         'no_trailing_comma_in_singleline_array' => true,
@@ -36,14 +46,14 @@ return PhpCsFixer\Config::create()
         'ordered_imports' => true,
         'single_quote' => true,
         'no_empty_statement' => true,
-        'no_extra_consecutive_blank_lines' => true,
+        'no_extra_blank_lines' => true,
         'phpdoc_no_package' => true,
         'phpdoc_scalar' => true,
         'no_blank_lines_after_phpdoc' => true,
         'array_syntax' => ['syntax' => 'short'],
         'whitespace_after_comma_in_array' => true,
         'function_typehint_space' => true,
-        'hash_to_slash_comment' => true,
+        'single_line_comment_style' => true,
         'no_alias_functions' => true,
         'lowercase_cast' => true,
         'no_leading_namespace_whitespace' => true,
@@ -52,10 +62,6 @@ return PhpCsFixer\Config::create()
         'no_short_bool_cast' => true,
         'no_unneeded_control_parentheses' => true
     ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->exclude('vendor')
-            ->exclude('node_modules')
-            ->exclude('public')
-            ->in(__DIR__)
-    );
+    ->setFinder($finder);
+
+return $config;
