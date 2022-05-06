2dec7f91ffce4aad4790923cfb39c60d7fa97ad4
Timo Poppinga <timo.poppinga@zdrei.com>
2022-05-06 07:57:02 +0200

    use typo3 core funktion to find path to public resources

--- Classes/Utility/PathUtility.php
+++ Classes/Utility/PathUtility.php
@@ -3,17 +3,12 @@ declare(strict_types=1);
 namespace YoastSeoForTypo3\YoastSeo\Utility;
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\PathUtility as TYPO3PathUtility;
 
 class PathUtility
 {
     public static function getPublicPathToResources(): string
     {
-        $isNewComposerMode = ComposerUtility::isNewComposerMode();
-        if($isNewComposerMode === true) {
-            return '/_assets/083b2b9e55c480021a1a9e04529d44d1';
-        }
-        $publicResourcesPath =
-            \TYPO3\CMS\Core\Utility\PathUtility::getAbsoluteWebPath(ExtensionManagementUtility::extPath('yoast_seo')) . 'Resources/Public';
-        return $publicResourcesPath;
+        return TYPO3PathUtility::getAbsoluteWebPath(ExtensionManagementUtility::extPath('yoast_seo') . 'Resources/Public/');
     }
 }
