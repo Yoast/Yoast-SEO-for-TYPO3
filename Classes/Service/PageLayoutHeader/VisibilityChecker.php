5e4eeb2bfba740ef620f473a80c5f9d17dc8a54e
--system <info@online-gamer.nl>
2024-12-18 20:27:43 +0100

    [TASK] Moved hideYoastInPageModule check within VisibilityChecker to separate method

--- Classes/Service/PageLayoutHeader/VisibilityChecker.php
+++ Classes/Service/PageLayoutHeader/VisibilityChecker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace YoastSeoForTypo3\YoastSeo\Service\PageLayoutHeader;
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use YoastSeoForTypo3\YoastSeo\Traits\BackendUserTrait;
 use YoastSeoForTypo3\YoastSeo\Utility\YoastUtility;
 
@@ -31,23 +32,29 @@ class VisibilityChecker
     protected function isSnippetPreviewEnabled(int $pageId, array $pageRecord): bool
     {
         $backendUser = $this->getBackendUser();
-
         if (!$backendUser->check('non_exclude_fields', 'pages:tx_yoastseo_snippetpreview')) {
             return false;
         }
 
-        if ((bool)($backendUser->uc['hideYoastInPageModule'] ?? false)) {
+        if ($this->hideFromUserSettings($backendUser)) {
             return false;
         }
 
         $pageTsConfig = $this->getPageTsConfig($pageId);
-        if (isset($pageTsConfig['mod.']['web_SeoPlugin.']['disableSnippetPreview'])
-            && (int)$pageTsConfig['mod.']['web_SeoPlugin.']['disableSnippetPreview'] === 1
-        ) {
+        if ((int)($pageTsConfig['mod.']['web_SeoPlugin.']['disableSnippetPreview'] ?? 0) === 1) {
             return false;
         }
 
         return !((bool)($pageRecord['tx_yoastseo_hide_snippet_preview'] ?? false));
+    }
+
+    protected function hideFromUserSettings(BackendUserAuthentication $backendUser): bool
+    {
+        if (isset($backendUser->uc['hideYoastInPageModule'])) {
+            return (bool)$backendUser->uc['hideYoastInPageModule'];
+        }
+
+        return false;
     }
 
     /**
