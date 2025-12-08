c301ce4d5058b17c064b8c848d6e14445c0c04e4
Gabriel Kaufmann <info@typoworx.com>
2025-12-08 09:30:06 +0100

    [feature/fix][#621] proposal to bypass cloudflare issues with yoast preview

--- Classes/Service/Preview/UrlContentFetcher.php
+++ Classes/Service/Preview/UrlContentFetcher.php
@@ -17,12 +17,30 @@ class UrlContentFetcher
 
     public function fetch(string $uriToCheck): string
     {
+        $parsedUri = parse_url($uriToCheck);
+
+        $scheme = $parsedUri['scheme'] ?? 'http';
+        $host   = $parsedUri['host'];
+        $port   = $parsedUri['port'] ?? ($scheme === 'https' ? '443' : '80');
+        $path   = $parsedUri['path'] ?? '';
+        $query  = isset($parsedUri['query'])    ? '?' . $parsedUri['query'] : '';
+        $frag   = isset($parsedUri['fragment']) ? '#' . $parsedUri['fragment'] : '';
+
+        $requestUrl = sprintf(
+            '%s://%s%s%s%s',
+            'http', '127.0.0.1', $path, $query, $frag
+        );
+
         try {
             $response = $this->requestFactory->request(
-                $uriToCheck,
+                $requestUrl,
                 'GET',
                 [
                     'headers' => [
+                        'Host' => $host,
+                        'X-Forwarded-For'      => $_SERVER['HTTP_X_FORWARDED_FOR'] ?? 'localhost',
+                        'X-Forwarded-Proto'    => $scheme,
+                        'X-Forwarded-Port'     => $port,
                         'X-Yoast-Page-Request' => GeneralUtility::hmac(
                             $uriToCheck
                         ),
